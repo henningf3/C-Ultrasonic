7de00a95972b3746d17fe67b1d2d4d5f6fd2b3af
henningf3 <henning.dierks03@gmail.com>
2023-01-26 16:59:54 +0100

    final

--- MRT_Ultrasonic.c
+++ MRT_Ultrasonic.c
@@ -17,22 +17,14 @@
  * @param distance_mm Distance in mm.
  * @return int High time in ms.
  */
-int determine_high_time(int distance_mm)
-{
-    if (distance_mm < DIST1)
-    {
+int determine_high_time(int distance_mm) {
+    if (distance_mm < DIST1) {
         return 300;
-    }
-    else if (distance_mm < DIST2 && distance_mm >= DIST1)
-    {
-        return (int)((double)distance_mm * 2 / 9 + 277.7);
-    }
-    else if (distance_mm < DIST3 && distance_mm >= DIST2)
-    {
-        return (int)((double)distance_mm * 0.5);
-    }
-    else
-    {
+    } else if (distance_mm < DIST2 && distance_mm >= DIST1) {
+        return (int) ((double) distance_mm * 2 / 9 + 277.7);
+    } else if (distance_mm < DIST3 && distance_mm >= DIST2) {
+        return (int) ((double) distance_mm * 0.5);
+    } else {
         return 1000;
     }
 }
@@ -43,22 +35,14 @@ int determine_high_time(int distance_mm)
  * @param distance_mm Distance in mm.
  * @return int Low time in ms.
  */
-int determine_low_time(int distance_mm)
-{
-    if (distance_mm < DIST1)
-    {
+int determine_low_time(int distance_mm) {
+    if (distance_mm < DIST1) {
         return 300;
-    }
-    else if (distance_mm < DIST2 && distance_mm >= DIST1)
-    {
-        return (int)((double)distance_mm * 4 / 9 + 255.6);
-    }
-    else if (distance_mm < DIST3 && distance_mm >= DIST2)
-    {
-        return (int)((double)distance_mm * 13 / 10 - 600);
-    }
-    else
-    {
+    } else if (distance_mm < DIST2 && distance_mm >= DIST1) {
+        return (int) ((double) distance_mm * 4 / 9 + 255.6);
+    } else if (distance_mm < DIST3 && distance_mm >= DIST2) {
+        return (int) ((double) distance_mm * 13 / 10 - 600);
+    } else {
         return 2000;
     }
 }
@@ -69,8 +53,7 @@ int determine_low_time(int distance_mm)
  * @param slice_num Pointer to the slice number.
  * @param chan Pointer to the channel number.
  */
-void init(uint *slice_num, uint *chan)
-{
+void init(uint *slice_num, uint *chan) {
     // GPIO init:
     gpio_init(TRIGGER_PIN);
     gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
@@ -81,7 +64,6 @@ void init(uint *slice_num, uint *chan)
     *slice_num = pwm_gpio_to_slice_num(PWM_PIN);
     *chan = pwm_gpio_to_channel(PWM_PIN);
     pwm_set_wrap(*slice_num, WRAP);
-    pwm_set_chan_level(*slice_num, *chan, WRAP);
     pwm_set_enabled(*slice_num, true);
 }
 
@@ -90,28 +72,24 @@ void init(uint *slice_num, uint *chan)
  *
  * @return int Distance in mm.
  */
-int read_buzzer()
-{
+int readSensor() {
     gpio_put(TRIGGER_PIN, 0);
-    sleep_ms(1); // ensure Pin is really low
+    sleep_us(3); // ensure Pin is really low
     gpio_put(TRIGGER_PIN, 1);
-    sleep_ms(10);
+    sleep_us(10);
     gpio_put(TRIGGER_PIN, 0);
     uint64_t start = time_us_64();
     // wait until echo pin is high
-    while (gpio_get(ECHO_PIN) == 0)
-    {
-        if (time_us_64() - start > 10000000)
-        {
+    while (gpio_get(ECHO_PIN) == 0) {
+        if (time_us_64() - start > 10000000) {
             return -1;
         }
     }
-    while (gpio_get(ECHO_PIN) == 1)
-    {
+    while (gpio_get(ECHO_PIN) == 1) {
         continue;
     }
     uint64_t duration = time_us_64() - start;
-    return (int)((double)duration / 2 * 0.343); // value in mm (0.5 time for half way * speed of sound)
+    return (int) ((double) duration / 2 * 343); // value in mm (0.5 time for half way * speed of sound)
 }
 
 /**
@@ -120,33 +98,25 @@ int read_buzzer()
  * @param distance_mm Distance in mm.
  * @param pwm_compare Pointer to the pwm compare value.
  */
-void set_volume(int distance_mm, float *pwm_compare)
-{
-    if (distance_mm < DIST1)
-    {
-        *pwm_compare = 1 / 5700 * (float)DIST1 + 0.5
+void set_volume(int distance_mm, float *pwm_compare) {
+    if (distance_mm < DIST1) {
+        *pwm_compare = 1 / 5700 * (float) DIST1 + 0.5;
+    } else if (distance_mm > DIST3) {
+        *pwm_compare = 1 / 5700 * (float) DIST3 + 0.5;
     }
-    else if (distance_mm > DIST3)
-    {
-        *pwm_compare = 1 / 5700 * (float)DIST3 + 0.5;
-    }
-
-    *pwm_compare = 1 / 5700 * (float)distance_mm + 0.5;
+    *pwm_compare = 1 / 5700 * (float) distance_mm + 0.5;
 }
 
 /**
  * @brief Main function.
  */
-int main()
-{
+int main() {
     int distance_mm = 1000;
     uint slice_num, chan;
     float pwm_compare;
-
     init(&slice_num, &chan);
-    while (true)
-    {
-        distance_mm = read_buzzer();
+    while (true) {
+        distance_mm = readSensor();
         set_volume(distance_mm, &pwm_compare);
         pwm_set_chan_level(slice_num, PWM_CHAN_A, WRAP * pwm_compare);
         sleep_ms(determine_high_time(distance_mm));
